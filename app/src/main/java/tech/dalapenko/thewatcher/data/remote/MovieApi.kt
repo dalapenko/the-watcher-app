a54eb445668f51fe4dc5754b2e8fddf7faa800e1
Dmitrii Lapenko <hwoaran92@mail.ru>
2023-03-26 23:26:45 +0300

    move default api params to const

--- app/src/main/java/tech/dalapenko/thewatcher/data/remote/MovieApi.kt
+++ app/src/main/java/tech/dalapenko/thewatcher/data/remote/MovieApi.kt
@@ -9,24 +9,29 @@ import tech.dalapenko.thewatcher.data.model.Page
 
 interface MovieApi {
 
+    companion object {
+        const val DEFAULT_LOCALE = "ru-RU"
+        const val DEFAULT_PAGE = 1
+    }
+
     @GET("movie/now_playing")
     fun getNowPlaying(
-        @Query("page") page: Int = 1,
-        @Query("language") language: String = "ru-RU",
+        @Query("page") page: Int = DEFAULT_PAGE,
+        @Query("language") language: String = DEFAULT_LOCALE,
         @Query("api_key") apiKey: String = BuildConfig.TMDB_AUTH_TOKEN
     ): Call<Page<Movie>>
 
     @GET("movie/popular")
     fun getPopular(
-        @Query("page") page: Int = 1,
-        @Query("language") language: String = "ru-RU",
+        @Query("page") page: Int = DEFAULT_PAGE,
+        @Query("language") language: String = DEFAULT_LOCALE,
         @Query("api_key") apiKey: String = BuildConfig.TMDB_AUTH_TOKEN
     ): Call<Page<Movie>>
 
     @GET("movie/top_rated")
     fun getTopRated(
-        @Query("page") page: Int = 1,
-        @Query("language") language: String = "ru-RU",
+        @Query("page") page: Int = DEFAULT_PAGE,
+        @Query("language") language: String = DEFAULT_LOCALE,
         @Query("api_key") apiKey: String = BuildConfig.TMDB_AUTH_TOKEN
     ): Call<Page<Movie>>
 }

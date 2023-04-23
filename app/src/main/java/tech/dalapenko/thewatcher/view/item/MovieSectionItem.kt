6cec88f8d3953ec1028d8fcb11ccf51461ad9fb0
Dmitrii Lapenko <hwoaran92@mail.ru>
2023-04-23 18:11:31 +0300

    implement save scroll position for inner recycler view

--- app/src/main/java/tech/dalapenko/thewatcher/view/item/MovieSectionItem.kt
+++ app/src/main/java/tech/dalapenko/thewatcher/view/item/MovieSectionItem.kt
@@ -2,7 +2,10 @@ package tech.dalapenko.thewatcher.view.item
 
 import android.view.View
 import androidx.annotation.StringRes
-import androidx.recyclerview.widget.RecyclerView.AdapterDataObserver
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import androidx.viewbinding.ViewBinding
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.viewbinding.BindableItem
@@ -14,15 +17,22 @@ class MovieSectionItem(
     @StringRes private val title: Int
 ) : BindableItem<ItemSectionBinding>() {
 
+    private val recyclerView: RecyclerView? = null
     private val adapter = GroupAdapter<GroupieViewHolder>()
+    private val scrollListener = SaveStateOnScrollListener()
+    private val sectionVisibilityDataObserver = SectionVisibilityDataObserver(adapter)
+
 
     override fun bind(viewBinding: ItemSectionBinding, position: Int) {
-        setSectionVisibility(viewBinding)
+        with(sectionVisibilityDataObserver) {
+            this.viewBinding = viewBinding
+            setSectionVisibility()
+        }
 
         viewBinding.sectionTitle.setText(title)
-        viewBinding.sectionRecyclerView.adapter = adapter.apply {
-            registerAdapterDataObserver(getSectionVisibilityDataObserver(viewBinding))
-        }
+        viewBinding.sectionRecyclerView.addOnScrollListener(scrollListener)
+        viewBinding.sectionRecyclerView.scrollToPosition(scrollListener.currentPosition)
+        viewBinding.sectionRecyclerView.adapter = adapter
     }
 
     override fun getLayout(): Int = R.layout.item_section
@@ -33,24 +43,45 @@ class MovieSectionItem(
 
     fun updateMovies(movieList: List<Movie>) {
         adapter.update(movieList.map(MovieItem::fromMovie))
+        recyclerView?.scrollToPosition(scrollListener.currentPosition)
     }
 
-    private fun getSectionVisibilityDataObserver(viewBinding: ItemSectionBinding): AdapterDataObserver {
-        return object : AdapterDataObserver() {
+    private class SectionVisibilityDataObserver<VH : ViewHolder>(
+        private val adapter: Adapter<VH>
+    ) : AdapterDataObserver() {
 
-            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
-                super.onItemRangeInserted(positionStart, itemCount)
-                setSectionVisibility(viewBinding)
-            }
+        var viewBinding: ViewBinding? = null
 
-            override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
-                super.onItemRangeRemoved(positionStart, itemCount)
-                setSectionVisibility(viewBinding)
+        init {
+            adapter.registerAdapterDataObserver(this)
+        }
+
+        override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+            super.onItemRangeInserted(positionStart, itemCount)
+            setSectionVisibility()
+        }
+
+        override fun onItemRangeRemoved(positionStart: Int, itemCount: Int) {
+            super.onItemRangeRemoved(positionStart, itemCount)
+            setSectionVisibility()
+        }
+
+        fun setSectionVisibility() {
+            viewBinding?.let {
+                it.root.visibility = if (adapter.itemCount == 0) View.GONE else View.VISIBLE
             }
         }
     }
 
-    private fun setSectionVisibility(viewBinding: ItemSectionBinding) {
-        viewBinding.root.visibility = if (adapter.itemCount == 0) View.GONE else View.VISIBLE
+    private class SaveStateOnScrollListener : OnScrollListener() {
+        var currentPosition = 0
+            private set
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            (recyclerView.layoutManager as LinearLayoutManager?)
+                ?.findFirstCompletelyVisibleItemPosition()?.let {  currentPosition = it }
+        }
     }
+
 }
